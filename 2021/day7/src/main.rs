ab176b0a572dfe2bf4159c577f3f0e07aaa1ead4
yuetongyu <yuetongyu@bytedance.com>
2021-12-14 18:12:03 +0800

    add day7 part two

--- 2021/day7/src/main.rs
+++ 2021/day7/src/main.rs
@@ -16,7 +16,24 @@ fn main() -> anyhow::Result<()> {
         .collect();
     positions.sort();
     part_one(&positions);
+    part_two(&positions);
     Ok(())
+}
+
+fn part_two(positions: &[i32]) {
+    let sum = positions.iter().fold(0, |acc, x| acc + x);
+    let init = sum as f64 / positions.len() as f64;
+    let p1 = (init + 0.5).round();
+    let p2 = (init - 0.5).round();
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+    for i in positions {
+        ans1 += fuel(*i as i64 - p1 as i64);
+        ans2 += fuel(*i as i64 - p2 as i64);
+        // dbg!(i, ans1);
+    }
+    dbg!(ans1, ans2);
+    dbg!(ans1.min(ans2));
 }
 
 fn part_one(positions: &[i32]) {
@@ -33,4 +50,12 @@ fn part_one(positions: &[i32]) {
         ans = ans.min(sum);
     }
     dbg!(ans);
+}
+
+fn fuel(dis: i64) -> i64 {
+    let mut dis = dis;
+    if dis < 0 {
+        dis = -dis;
+    }
+    (1 + dis) * dis / 2
 }
